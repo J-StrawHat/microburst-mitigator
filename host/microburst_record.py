463edf0b6f720ad8b2e3a69f2d5559ef2de367a4
J-StrawHat <422148166@qq.com>
2023-03-30 11:10:05 +0800

    优化队列长度记录的程序

--- host/microburst_record.py
+++ host/microburst_record.py
@@ -6,10 +6,9 @@ from flowinfo import *
 from scapy.all import sniff, get_if_list, get_if_hwaddr
 from scapy.all import Ether, IP, UDP, TCP, Raw, ls
 
-curdate = time.strftime("%m%d_%H", time.localtime())
-fa_dir = 'log'
+curdate = time.strftime("%m%d-%H-%M", time.localtime())
+fa_dir = 'host/log'
 output_file = fa_dir + '/%s.csv' % curdate
-ts_list, qdepth_list = [], []
 
 def get_if():
     iface=None
@@ -24,42 +23,44 @@ def get_if():
 
 def handle_pkt(pkt):
     if IP in pkt:
-        # print_flowinfo(pkt)
-        record_depth(pkt, tscale = 100)
+        #print_flowinfo(pkt)
+        record_depth(pkt, tscale = 1)
         sys.stdout.flush()
 
 def print_flowinfo(pkt, selected = False):
     if FLOWINFO in pkt:
         print(pkt[FLOWINFO].deq_qdepth)
+    
 
 def record_depth(pkt, tscale = 100):
     if FLOWINFO not in pkt:
         return
     if pkt[FLOWINFO].egress_ts % tscale != 0:
         return
-    qdepth_list.append(pkt[FLOWINFO].deq_qdepth)
-    ts_list.append(pkt[FLOWINFO].egress_ts)
 
-    if pkt[FLOWINFO].egress_ts % (tscale * 1000) == 0:
-        with open(output_file, mode='a') as file:
-            writer = csv.writer(file)
-            writer.writerows(zip(ts_list, qdepth_list))
-        ts_list.clear()
-        qdepth_list.clear()
+    print("%d,%d,%d" % (pkt[FLOWINFO].egress_ts, pkt[FLOWINFO].deq_qdepth, len(pkt)))
+
+    with open(output_file, mode='a') as file:
+        writer = csv.writer(file)
+        writer.writerow((pkt[FLOWINFO].egress_ts, pkt[FLOWINFO].deq_qdepth))
+
 
 def main():
     ifaces = [i for i in os.listdir('/sys/class/net/') if 'eth' in i]
     iface = ifaces[0]
-    print("sniffing on %s" % iface)
+    #print("sniffing on %s" % iface)
     
     sys.stdout.flush()                      # 显式地让缓冲区的内容输出
 
-    if not os.path.exists('fa_dir'):
+    if not os.path.exists(fa_dir):
         os.makedirs(fa_dir)
 
+    
     with open(output_file, 'w', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(['timestamp(us)', 'Queue Length(number of packets)'])
+        writer.writerow(['timestamp(us)', 'Queue Depth(number of packets)'])
+    
+    #print("timestamp(us),Queue Length(number of packets)")
 
     sniff(iface = iface,      # 指定要在哪个网络接口上进行抓包
           prn = lambda x: handle_pkt(x))    #每当一个符合filter的报文被探测到时，就会执行回调函数
